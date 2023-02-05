de6495d404cbf379df49d2aa4a935a7c15b5e3c4
kaiqueBellmont <kaiquebellmont@gmail.com>
2023-02-05 16:48:49 -0300

    starting the header component

---
+++ shop/components/header/Top.js
@@ -0,0 +1,11 @@
+import styles from './styles.module.scss'
+
+export default function Top() {
+  return (
+    <div className={styles.top}>
+        <div className={styles.top__container}>
+            
+        </div>
+    </div>
+  )
+}
--- shop/pages/_app.js
+++ shop/pages/_app.js
@@ -4,15 +4,23 @@ import { Provider } from 'react-redux'
 import store from '../store/index'
 import { PersistGate } from 'redux-persist/integration/react'
 import { persistStore } from 'redux-persist'
-
+import Head from 'next/head'
 let persistor = persistStore(store);
 function App({ Component, pageProps }) {
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <Component {...pageProps} />
-      </PersistGate>
-    </Provider>
+    <>
+      <Head>
+        <title>Shoppay</title>
+        <meta name="description" content="Shoppay-online shopping for all of your needs." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <Component {...pageProps} />
+        </PersistGate>
+      </Provider>
+    </>
   )
 }
 
