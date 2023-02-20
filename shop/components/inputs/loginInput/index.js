96060e2f49f0fa4329ff344cab58a2abf9a62d8b
kaiqueBellmont <kaiquebellmont@gmail.com>
2023-02-20 16:52:05 -0300

    Finishing the signin page 1/2

--- shop/components/inputs/loginInput/index.js
+++ shop/components/inputs/loginInput/index.js
@@ -1,15 +1,15 @@
-import styles from './styles.module.scss'
-import { BiUser } from 'react-icons/bi'
-import { SiMinutemailer } from 'react-icons/si'
-import { IoKeyOutline } from 'react-icons/io5'
-import { useField } from 'formik'
-
-
-
+import styles from "./styles.module.scss";
+import { BiUser } from "react-icons/bi";
+import { SiMinutemailer } from "react-icons/si";
+import { IoKeyOutline } from "react-icons/io5";
+import { ErrorMessage, useField } from "formik";
 export default function LoginInput({ icon, placeholder, ...props }) {
-  const [field, meta] = useField()
+  const [field, meta] = useField(props);
   return (
-    <div className={styles.input}>
+    <div
+      className={`${styles.input} ${meta.touched && meta.error ? styles.error : ""
+        }`}
+    >
       {icon == "user" ? (
         <BiUser />
       ) : icon == "email" ? (
@@ -26,6 +26,12 @@ export default function LoginInput({ icon, placeholder, ...props }) {
         {...field}
         {...props}
       />
+      {meta.touched && meta.error && (
+        <div className={styles.error__popup}>
+          <span></span>
+          <ErrorMessage name={field.name} />
+        </div>
+      )}
     </div>
-  )
+  );
 }
--- shop/pages/signin.js
+++ shop/pages/signin.js
@@ -1,6 +1,8 @@
 import Header from '../components/header'
 import Footer from '../components/footer'
 import LoginInput from '../components/inputs/loginInput/index'
+import { signIn } from 'next-auth/react';
+
 
 import { BiLeftArrowAlt } from 'react-icons/bi'
 
@@ -9,15 +11,17 @@ import Link from 'next/link'
 import { Formik, Form } from 'formik'
 import * as Yup from 'yup'
 import { useState } from 'react'
+import CircledIconBtn from '../components/buttons/circledIconBtn'
+import { getProviders } from 'next-auth/react'
 
-const initialValues = {
+const initialvalues = {
   login_email: "",
   login_password: "",
 }
 
 
-export default function signin() {
-  const [user, setUser] = useState(initialValues);
+export default function signin({ providers }) {
+  const [user, setUser] = useState(initialvalues);
   const { login_email, login_password } = user;
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -68,19 +72,45 @@ export default function signin() {
                   />
                   <LoginInput
                     type="password"
-                    name="login_passsword"
+                    name="login_password"
                     icon="password"
                     placeholder="Senha"
                     onChange={handleChange}
                   />
+                  <CircledIconBtn type="submit" text="Entrar" />
+                  <div className={styles.forgot}>
+                    <Link href="/forget">Esqueceu a senha?</Link>
+                  </div>
                 </Form>
               )}
             </Formik>
+            <div className={styles.login__socials}>
+              <span className={styles.or}>Ou Continue com</span>
+              <div className={styles.login__socials_wrap}>
+                {providers.map((provider) => (
+                  <div key={provider.name}>
+                    <button className={styles.social__btn} onClick={() => signIn(provider.id)}>
+                      <img src={`../../icons/${provider.id}.png`} alt="" />
+                      Entrar com {provider.name}
+                    </button>
+                  </div>
+                ))}
+              </div>
+            </div>
           </div>
         </div>
       </div>
-      <Footer country={"Barzil"} />
+      <Footer country={"Brazil"} />
 
     </>
   )
 }
+
+export async function getServerSideProps(context) {
+  const providers = Object.values(await getProviders());
+  return {
+    props: {
+      providers
+    },
+  }
+}  
